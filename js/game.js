a695928c04523c87007b472f3ba4d5593d261106
Maria Eduarda Tolentino <135777277+DudixT@users.noreply.github.com>
2025-04-15 00:11:55 +0000

    aa

--- js/game.js
+++ js/game.js
@@ -18,5 +18,19 @@ function novaPergunta() {
     respostaCorreta = calcularResposta(numeros.num1, numeros.num2, operacao);
 
     document.getElementById("pergunta").innerText = perguntaTexto;
-    
+    document.getElementById("resposta").value = "";
+    document.getElementById("resultado").innerText = "";
 }
+
+function gerarNumeros(modo) {
+    let num1 = Math.floor(Math,random() *10) + 1;
+    let num2 = Math.floor(Math,random() *10) + 1;
+
+    if (modo === 'desafio') {
+        num1 *= 2;
+        num2 *= 3;
+    }
+
+    return {num1, num2};
+}
+
